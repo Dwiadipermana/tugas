6e0ad90e8356d33e5d9c46bc886be0dbbd4fe744
Dwiadipermana <114789269+Dwiadipermana@users.noreply.github.com>
2023-10-05 21:33:01 +0700

    Add files via upload

---
+++ tugas 2.js
@@ -0,0 +1,9 @@
+const user ={
+    firstName: "Dwi",
+    lastName: "Adi",
+    age:21,
+    isMarried: false,
+
+};
+console.log(`Halo,nama saya ${user.firstName} ${user.lastName}`);
+console.log(`umur saya ${user.age}tahun`);
